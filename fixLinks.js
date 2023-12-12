2431b7aceeb6ce4bad6f25c1962f52a6fd6a045d
cperryoh <cperryoh@gmail.com>
2023-12-12 11:36:52 -0500

    comments

--- fixLinks.js
+++ fixLinks.js
@@ -1,12 +1,20 @@
 questions = document.getElementsByClassName("formulation clearfix");
 for (var quest of questions) {
+    //Check if image question
     if (quest.querySelector(".attachments") != null) {
+        //grab image and link
         var img = quest.querySelector(".attachments").querySelector("a");
         var link = img.href.replace("?forcedownload=1", "");
+        
+        //grab container of answer image
         var graderView = quest.parentElement.querySelector(".graderinfo").querySelector("img").parentElement;
         var image = graderView.querySelector("img").cloneNode(true);
+        
+        //style answer image
         graderView.querySelector('img').style.height = 300;
         graderView.querySelector('img').style.width = 'auto';
+        
+        //add either pdf or image depending on what was submitted
         if (link.includes(".png") || link.includes(".jpg")) {
             image.style.height = "auto";
             image.src = link;
